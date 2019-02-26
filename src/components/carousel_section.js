945ab88b2df7597747dec8dfd40bb20995fae540
adamcglee <adamcglee@gmail.com>
2019-02-26 01:00:07 -0800

    creating pc_code_challenge app, creating file structure, and adding necessary image files.

---
+++ src/components/carousel_section.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarouselSection = (props) => {
+    return ( 
+        <div>
+            Carousel Section
+        </div>
+     );
+}
+ 
+export default CarouselSection;
---
+++ src/components/hero_section.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroSection = (props) => {
+    return ( 
+        <div>
+            Hero Section
+        </div>
+     );
+}
+ 
+export default HeroSection;
