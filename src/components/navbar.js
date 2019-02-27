db5e1c089d4a7c2b2193a3bb5aad1ef47d48e02c
adamcglee <adamcglee@gmail.com>
2019-02-26 19:33:25 -0800

    more preliminary work to set up the project

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styled from "styled-components";
 
 const NavBar = (props) => {
     return ( 
-        <div>
-            Navbar
+        <div className="navbar">
+            
         </div>
      );
 }
